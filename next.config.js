31913ed52753b249b58fa851bb66ed64d6cd4d49
rasikasrimal <77890327+rasikasrimal@users.noreply.github.com>
2025-10-02 14:27:23 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
-const config = {
-  experimental: {
-    typedRoutes: true,
-  },
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  fallbacks: { document: "/offline.html" }
+});
 
-module.exports = config;
+module.exports = withPWA({
+  output: "export",           // <= add this line once
+  experimental: {
+    typedRoutes: true
+  }
+});
